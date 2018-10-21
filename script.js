6e84e050dc03db634eb7c2e30795a05cbd97122e
amstanley1 <amstnly@gmail.com>
2018-10-21 15:56:05 -0400

    Added functionality to show project descriptions

--- script.js
+++ script.js
@@ -122,13 +122,20 @@
   }
 
   function addProjectListeners() {
-    const projects = document.querySelectorAll('#project-grid article');
+    const descriptionButtons = document.querySelectorAll('.showDescription');
 
-    for (let i = 0; i < projects.length; i++) {
-      project[i].addEventListener('click', function() {
-
-        let projectDescription = project[i].querySelector('#projectDescription');
-        projectDescription.style.display = "block";
+    for (let i = 0; i < descriptionButtons.length; i++) {
+     descriptionButtons[i].addEventListener('click', function() {
+        let projectDescriptions = document.querySelectorAll('p.projectDescription');
+        projectDescriptions[i].classList.toggle('openDescription');
+        projectDescriptions[i].classList.toggle('closeDescription');
+        if (descriptionButtons[i].innerText === "Hide Description") {
+          descriptionButtons[i].innerText = "Show Description";
+         } else {
+          descriptionButtons[i].innerText = "Hide Description";
+         }
+        
+      
       });
     };
 
